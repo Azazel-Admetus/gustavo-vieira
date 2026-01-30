66cdc1d834e8fa6b72aba0a2949231ee978f710c
azazel_admetus <gustavodsv7184@gmail.com>
2026-01-29 21:18:44 -0400

    removi mais algumas dependências da API do base44 que estava na pasta lib

--- src/lib/app-params.jsx
+++ src/lib/app-params.jsx
@@ -6,11 +6,11 @@ const toSnakeCase = (str) => {
 	return str.replace(/([A-Z])/g, '_$1').toLowerCase();
 }
 
-const getAppParamValue = (paramName, { defaultValue = undefined, removeFromUrl = false } = {}) => {
+const getParamValue = (paramName, { defaultValue = undefined, removeFromUrl = false } = {}) => {
 	if (isNode) {
 		return defaultValue;
 	}
-	const storageKey = `base44_${toSnakeCase(paramName)}`;
+	const storageKey = `app_${toSnakeCase(paramName)}`;
 	const urlParams = new URLSearchParams(window.location.search);
 	const searchParam = urlParams.get(paramName);
 	if (removeFromUrl) {
@@ -34,21 +34,14 @@ const getAppParamValue = (paramName, { defaultValue = undefined, removeFromUrl =
 	return null;
 }
 
-const getAppParams = () => {
-	if (getAppParamValue("clear_access_token") === 'true') {
-		storage.removeItem('base44_access_token');
-		storage.removeItem('token');
-	}
+const getParams = () => {
 	return {
-		appId: getAppParamValue("app_id", { defaultValue: import.meta.env.VITE_BASE44_APP_ID }),
-		token: getAppParamValue("access_token", { removeFromUrl: true }),
-		fromUrl: getAppParamValue("from_url", { defaultValue: window.location.href }),
-		functionsVersion: getAppParamValue("functions_version", { defaultValue: import.meta.env.VITE_BASE44_FUNCTIONS_VERSION }),
-		appBaseUrl: getAppParamValue("app_base_url", { defaultValue: import.meta.env.VITE_BASE44_APP_BASE_URL }),
+		appId: getParamValue("app_id"),
+		token: getParamValue("access_token", { removeFromUrl: true }),
+		fromUrl: getParamValue("from_url", { defaultValue: typeof window !== 'undefined' ? window.location.href : '' }),
 	}
 }
 
-
 export const appParams = {
-	...getAppParams()
+	...getParams()
 }
